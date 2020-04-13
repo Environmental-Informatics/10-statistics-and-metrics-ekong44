b5e02b4508070572ab0ac226cd25b893f893390f
Eric Kong <kong44@scholar-fe05.rcac.purdue.edu>
2020-04-13 16:50:52 -0400

    Trying to fix the were it is trying to add a integer and string

--- program_10.py
+++ program_10.py
@@ -104,14 +104,14 @@ def CalcRBindex(Qvalues):
        routine returns the RBindex value for the given data array."""
     
     # Qvalues is the series of streamflow values
-    storage = int(0) # initalized as zero, this variable stores the value from  
+    z = 0 # initalized as zero, this variable stores the value from  
                 # the previous day-to-day change calulation so it can be added during the summation
     
     remove_nan = len(Qvalues.dropna())
     if remove_nan > 0:   # run if the length of the flow values, after nan's are removed is greater than 0
         for position in range(1, remove_nan): # loop for all values
-            storage = storage + abs(Qvalues.iloc[position-1] - Qvalues.iloc[position]) # abs value of day-to-day change  
-        RBindex = storage / sum(Qvalues.dropna()) # summed day-to-day changes divided by sum of flow         
+            z = z + abs(Qvalues.iloc[position-1] - Qvalues.iloc[position]) # abs value of day-to-day change  
+        RBindex = z / sum(Qvalues.dropna()) # summed day-to-day changes divided by sum of flow         
     else: 
             RBindex = np.nan    # combatting the divide by zero error that occurs because of the nan values    
     return ( RBindex )
